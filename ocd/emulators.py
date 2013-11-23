0284ec6316030cb00003e40879d519565e3df5db
ana-balica <ana.balica@gmail.com>
2013-11-23 18:50:23 +0100

    move emulators to a new file - not yet used

---
+++ ocd/emulators.py
@@ -0,0 +1,16 @@
+import logging
+
+
+class I2C_emulator(object):
+    def __init__(self):
+        logging.info("i2c emulator started")
+
+
+class SPI_emulator(object):
+    def __init__(self):
+        logging.info("spi emulator started")
+
+
+class OneWire_emulator(object):
+    def __init__(self):
+        logging.info("spi emulator started")
